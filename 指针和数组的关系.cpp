4fe2553577dad240467ab2e5bcf808483d44debe
‘15071832337’ <1418868984@qq.com>
2022-07-22 19:12:22 +0800

    已修改

--- 指针和数组的关系.cpp
+++ 指针和数组的关系.cpp
@@ -2,30 +2,33 @@
  * @Author: ‘15071832337’ ‘1418868984@qq.com’
  * @Date: 2022-07-22 11:39:19
  * @LastEditors: ‘15071832337’ ‘1418868984@qq.com’
- * @LastEditTime: 2022-07-22 18:57:48
+ * @LastEditTime: 2022-07-22 19:11:05
  * @FilePath: \vscode\1.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-#include<iostream>
+#include <iostream>
+ 
 using namespace std;
 const int MAX = 3;
-int main(int argc, char const *argv[])
+ 
+int main ()
 {
-    a1(); 
-    return 0;
-}
-
-void a1(){   
-    int arry[MAX] = {10,20,30};
-    int *ptr;
-    ptr=arry;
-    for (int i = 0; i < MAX; i++)
-    {
-       cout<<"arry["<<i<<"]的内存地址为:";
-       cout<<ptr<<endl;
-       ptr=ptr+1;
-    }
-    
+   int  var[MAX] = {10, 100, 200};
+   int  *ptr;
+ 
+   // 指针中的数组地址
+   ptr = var;
+   for (int i = 0; i < MAX; i++)
+   {
+      cout << "var[" << i << "] the address is ";
+      cout << ptr << endl;
+      //cout << "var[" << i << "] the number is ";
+      //cout << *ptr << endl;
+ 
+      // 移动到下一个位置
+      ptr=ptr+1;
+   }
+   return 0;
 }
 
 
