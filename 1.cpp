8fd61102b198346d29fcadab5a53b952600d0fea
‘15071832337’ <1418868984@qq.com>
2022-07-22 11:47:15 +0800

    已经修改

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,7 @@
  * @Author: ‘15071832337’ ‘1418868984@qq.com’
  * @Date: 2022-07-22 11:39:19
  * @LastEditors: ‘15071832337’ ‘1418868984@qq.com’
- * @LastEditTime: 2022-07-22 11:42:04
+ * @LastEditTime: 2022-07-22 11:45:20
  * @FilePath: \vscode\1.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,4 +13,7 @@ int main(int argc, char const *argv[])
 {
     cout<<"Hello World"<<endl;   
     return 0;
+}
+void a(){
+    
 }
