43279712452a17ea6c6c0c2c6e70271a35e16afd
‘15071832337’ <1418868984@qq.com>
2022-07-22 19:20:31 +0800

    已经修改

--- 指针和数组的关系.cpp
+++ 指针和数组的关系.cpp
@@ -2,17 +2,24 @@
  * @Author: ‘15071832337’ ‘1418868984@qq.com’
  * @Date: 2022-07-22 11:39:19
  * @LastEditors: ‘15071832337’ ‘1418868984@qq.com’
- * @LastEditTime: 2022-07-22 19:11:05
+ * @LastEditTime: 2022-07-22 19:20:17
  * @FilePath: \vscode\1.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <iostream>
- 
+ void a1();
+ void a2();
 using namespace std;
 const int MAX = 3;
  
 int main ()
 {
+   a2();
+   return 0;
+}
+
+//利用指针偏移进行数组操作
+void a1(){
    int  var[MAX] = {10, 100, 200};
    int  *ptr;
  
@@ -22,13 +29,25 @@ int main ()
    {
       cout << "var[" << i << "] the address is ";
       cout << ptr << endl;
-      //cout << "var[" << i << "] the number is ";
-      //cout << *ptr << endl;
+      cout << "var[" << i << "] the number is ";
+      cout << *ptr << endl;
  
       // 移动到下一个位置
       ptr=ptr+1;
    }
-   return 0;
+}
+//利用数组本身进行数组操作
+
+void a2(){
+   int var[MAX] = {10,20,30};
+   for (int i = 0; i < MAX; i++)
+   {
+      cout << "var[" << i << "] the address is ";
+      cout<<&var[i]<<endl;
+      cout << "var[" << i << "] the number is ";
+      cout<<var[i]<<endl;
+   }
+   
 }
 
 
