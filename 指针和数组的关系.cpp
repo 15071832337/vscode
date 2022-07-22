0b4bece9aa3a11d9872e94ddd9cbc1455317e917
‘15071832337’ <1418868984@qq.com>
2022-07-22 18:59:45 +0800

    提交修改

---
+++ 指针和数组的关系.cpp
@@ -0,0 +1,32 @@
+/*
+ * @Author: ‘15071832337’ ‘1418868984@qq.com’
+ * @Date: 2022-07-22 11:39:19
+ * @LastEditors: ‘15071832337’ ‘1418868984@qq.com’
+ * @LastEditTime: 2022-07-22 18:57:48
+ * @FilePath: \vscode\1.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include<iostream>
+using namespace std;
+const int MAX = 3;
+int main(int argc, char const *argv[])
+{
+    a1(); 
+    return 0;
+}
+
+void a1(){   
+    int arry[MAX] = {10,20,30};
+    int *ptr;
+    ptr=arry;
+    for (int i = 0; i < MAX; i++)
+    {
+       cout<<"arry["<<i<<"]的内存地址为:";
+       cout<<ptr<<endl;
+       ptr=ptr+1;
+    }
+    
+}
+
+
+
