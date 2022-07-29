e2032d4220668fb0132fe7462d9f0f50d4f4719b
‘15071832337’ <1418868984@qq.com>
2022-07-29 09:29:31 +0800

    已提交

--- 指针和数组的关系.cpp
+++ 指针和数组的关系.cpp
@@ -2,7 +2,7 @@
  * @Author: ‘15071832337’ ‘1418868984@qq.com’
  * @Date: 2022-07-22 11:39:19
  * @LastEditors: ‘15071832337’ ‘1418868984@qq.com’
- * @LastEditTime: 2022-07-29 09:05:48
+ * @LastEditTime: 2022-07-29 09:27:17
  * @FilePath: \vscode\1.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -288,10 +288,11 @@ int TheValuEqualXiaBiao(int Arsize){
    {
       arr[i]=i;
    }
-   for (int i = 0; i < Arsize; i++)
+   for (int *p=arr; *p!=arr[Arsize] ; p++)
    {
-      cout<<arr[i]<<" ";
+      cout<<*p<<" ";
    }
+  
    return 0;
 }
 
